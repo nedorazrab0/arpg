7bb2fec8620da27997dd24b2cc714c6c26c8c52c
valenok <141725500+nedorazrab0@users.noreply.github.com>
2026-08-27 23:56:18 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,54 @@
+"use strict";
+// SPDX-License-Identifier: 0BSD
+
+const CACHE_NAME = "v0";
+const ASSETS = [
+    "/",
+    "/index.html",
+    "/404.html",
+    "/manifest.webmanifest",
+    "/scripts/init.js",
+    "/fonts/Inter-Regular-ASCII.woff2",
+    "/fonts/Inter-Bold-ASCII.woff2",
+    "/fonts/JetBrainsMono-Regular.woff2",
+    "/icons/icon.svg",
+    "/images/main-done.svg",
+    "/images/sub-done.svg",
+    "/images/show.svg",
+    "/images/hide.svg",
+    "/images/generate.svg",
+    "/images/copy.svg"
+];
+
+self.addEventListener(
+    "install", (event) => {
+        event.waitUntil(
+            caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+        );
+    }
+);
+
+self.addEventListener(
+    "activate", (event) => {
+        event.waitUntil(
+            caches.keys().then(
+                (keys) => Promise.all(
+                    keys.filter((key) => key !== CACHE_NAME)
+                        .map((key) => caches.delete(key))
+                )
+            )
+        );
+    }
+);
+
+self.addEventListener(
+    "fetch", (event) => {
+        event.respondWith(
+            caches.match(event.request).then(
+                (cachedResponse) => {
+                    return cachedResponse || fetch(event.request);
+                }
+            )
+        );
+    }
+);
